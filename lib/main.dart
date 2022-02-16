ff4a0b8a553bb03eb161dae2a4344b6e62e93aac
Apside <donnedelphine@outlook.fr>
2022-02-16 11:25:49 +0100

    add directory screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:library_exercice/register_screen.dart';
-import 'login_screen.dart';
+import 'package:library_exercice/screens/register_screen.dart';
+import 'screens/login_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
