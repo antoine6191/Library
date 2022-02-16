3f54f1c54a97305352989af70cb0a1055d0bfd6f
Apside <donnedelphine@outlook.fr>
2022-02-16 13:37:26 +0100

    form creation livre

---
+++ lib/screens/book_creation_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:library_exercice/models/book.dart';
+import 'package:library_exercice/services/book_service.dart';
+
+class BookCreationScreen extends StatelessWidget{
+
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _genreController = TextEditingController();
+  final TextEditingController _numeroIsbnController = TextEditingController();
+  final TextEditingController _numeroCodeBarreController = TextEditingController();
+  final BookService _bookService = BookService();
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+     body: Padding(
+       padding: EdgeInsets.all(30.0),
+       child: Form(
+         key: _formKey,
+         child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Titre'),
+                controller: _titleController,
+                validator: (text){
+                 text!.isEmpty ? 'veuiller entrer un nom' : _titleController.text = text;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Description'),
+                controller: _descriptionController,
+                validator: (text){
+                  text!.isEmpty ? 'veuiller entrer un nom' : _descriptionController.text = text;
+
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Genre'),
+                controller: _genreController,
+                validator: (text){
+                  text!.isEmpty ? 'veuiller entrer un nom' : _genreController.text = text;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Numéro Isbn'),
+                controller: _numeroIsbnController,
+                validator: (text){
+                  text!.isEmpty ? 'veuiller entrer un nom' : _numeroIsbnController.text = text;
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'Numéro code barre'),
+                controller: _numeroCodeBarreController,
+                validator: (text){
+                  text!.isEmpty ? 'veuiller entrer un nom' : _numeroCodeBarreController.text = text;
+                },
+              ),
+              OutlinedButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: Text('Annuler')),
+              ElevatedButton(onPressed: (){
+                if(_formKey.currentState!.validate()){
+                  Book book = Book(title: _titleController.text,description: _descriptionController.text);
+                  _bookService.addBook(book);
+                }
+              }, child: Text('Valider'))
+            ],
+         ),
+       ),
+     ),
+   );
+  }
+
+}
--- lib/services/book_service.dart
+++ lib/services/book_service.dart
@@ -4,9 +4,8 @@ import 'package:library_exercice/models/book.dart';
 class BookService{
   final CollectionReference _collectionReference = FirebaseFirestore.instance.collection('books');
 
-  addBook(){
-    Book book = Book(title: 'title', description: 'description');
-    _collectionReference.add(book.toJson());
+   Future<void> addBook(Book book){
+    return _collectionReference.add(book.toJson());
   }
   findAll(){
     _collectionReference.get().then((value) => value.docs.map((e) => e.data()).toList());
