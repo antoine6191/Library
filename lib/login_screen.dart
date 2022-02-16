5dd8783c6aa2319475ace291b7c4af2fbf9da348
antoine6191 <59445232+antoine6191@users.noreply.github.com>
2022-02-16 08:48:51 +0100

    addloginscreen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -2,11 +2,78 @@ import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
   static const route = "/login";
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
   String? name;
   String? password;
 
-  final _namecontroller = TextEditingController();
+  final _nameController = TextEditingController();
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Login")),
+      body: Center(
+          child: _buildLoginForm()),
+    );
+  }
+  Widget _buildLoginForm() {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(labelText: "Nom d'utilisateur"),
+              validator: (value) => value!.isEmpty ? 'Entrez votre nom' : null,
+            ),
+            TextFormField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'E-mail'),
+              validator: (value) => value!.isEmpty ? 'Entrez votre mdp' : null,
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                  foregroundColor: MaterialStateProperty.all(Colors.white)),
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  _validate();
+                  setState(() {
+                    //loggedIn = true;
+                    name = _nameController.text;
+                  });
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _validate() {
+    final form = _formKey.currentState;
+    if(!form!.validate()){
+      return;
+    }
+    final name = _nameController.text;
+    final email = _passwordController.text;
+
+    /*Navigator.of(context).pushReplacementNamed(
+      StopWatch.route,
+      arguments: name,
+    );*/
+  }
 }
